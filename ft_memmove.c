65b85c2b7749535bd0a9bd226b2cb1c77a1c103a
douglasjleite <djleite@outlook.com.br>
2026-05-21 12:42:54 -0300

    libft: ft_memmove function

---
+++ ft_memmove.c
@@ -0,0 +1,33 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			i;
+
+	if (!dest && !src)
+		return (0);
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+
+	if (d > s)
+	{
+		while (n > 0)
+		{
+			n--;
+			d[n] = s[n];
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
+}
