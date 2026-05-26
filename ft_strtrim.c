04213153b4383be7afb5af2e2445512ec1ece6e9
douglasjleite <djleite@outlook.com.br>
2026-05-25 22:18:12 -0300

    libft: ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*trimmed_str;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] != '\0' && ft_strchr(set, s1[start]))
+	{
+		start++;
+	}
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+	{
+		end--;
+	}
+	trimmed_str = ft_substr(s1, start, end - start);
+	return (trimmed_str);
+}
