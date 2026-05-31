65dbe4ebd18daf6d4bea2ea7d387720b58fa91f9
douglasjleite <djleite@outlook.com.br>
2026-05-31 18:31:09 -0300

    libft: ft_lstsize

---
+++ ft_lstsize.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+int ft_lstsize(t_list *lst)
+{
+	int	count;
+	t_list *curr;
+
+	count = 0;
+	curr = lst;
+	while (curr)
+	{
+		count++;
+		curr = curr->next;
+	}
+	return (count);
+}
