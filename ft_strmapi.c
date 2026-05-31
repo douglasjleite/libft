eb25e444480d3de2dcb144495a208ace87a22ab0
douglasjleite <djleite@outlook.com.br>
2026-05-31 13:44:36 -0300

    libft: ft_strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	i;
+	size_t	len;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
